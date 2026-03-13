891d8922285fc26dff9557e2d37b9f3b14ed653f
Kent1 <kent12710@gmail.com>
2026-03-13 15:32:32 +0700

    chuyển đổi từ login sang regis

--- app/src/main/java/com/example/doanltmb/LoginActivity.java
+++ app/src/main/java/com/example/doanltmb/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.doanltmb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -9,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Dòng này cực kỳ quan trọng: Nó kết nối code với giao diện XML của bạn
         setContentView(R.layout.activity_login);
+
+        TextView registerText = findViewById(R.id.registerText);
+        registerText.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/doanltmb/RegisterActivity.java
+++ app/src/main/java/com/example/doanltmb/RegisterActivity.java
@@ -1,6 +1,9 @@
 package com.example.doanltmb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -9,5 +12,11 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Dòng này cực kỳ quan trọng: Nó kết nối code với giao diện XML của bạn
         setContentView(R.layout.activity_regis);
+
+        TextView loginText = findViewById(R.id.loginText);
+        loginText.setOnClickListener(view -> {
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
